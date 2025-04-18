9be8cddffd93dc9ac710526bd2a84387eed7834d
juniorcintra <junior.cintra.developer@gmail.com>
2025-04-18 13:39:01 -0300

    feat: condicionais de rotas

--- src/server.js
+++ src/server.js
@@ -1,6 +1,24 @@
 import http from "node:http";
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res.end("Listagem de usuários");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return res.end("Criação de usuários");
+  }
+
+  if (method === "PUT" && url === "/users/1") {
+    return res.end("Alteração de usuários");
+  }
+
+  if (method === "DELETE" && url === "/users/1") {
+    return res.end("Exclusão de usuários");
+  }
+
   res.end("Hello World!");
 });
 
