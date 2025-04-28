2a0cf083b5c326807298175c460c423daeb58281
juniorcintra <junior.cintra.developer@gmail.com>
2025-04-28 20:07:32 -0300

    feat: :sparkles: Rotas da aplicação - Capturando query parameters
    Rotas da aplicação - Capturando query parameters

--- src/server.js
+++ src/server.js
@@ -2,19 +2,7 @@ import http from "node:http";
 
 import { json } from "./middlewares/json.js";
 import { routes } from "./routes.js";
-
-// Query Parameters: URL Stateful => Filtros, paginação, não-obrigatórios
-// Route Parameters: Identificação de recurso
-// Request Body: Envio de informações de um formulário (HTTPs)
-
-//http://localhost:3333/users?userId=1&name=Diego
-
-// GET http://localhost:3333/users/1
-// DELETE http: //localhost:3333/users/1
-
-// POST http://localhost:3333/users
-
-// Edição e remoção
+import { extractQueryParams } from "./utils/extract-query-params.js";
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -28,7 +16,10 @@ const server = http.createServer(async (req, res) => {
   if (route) {
     const routeParams = req.url.match(route.path);
 
-    req.params = { ...routeParams.groups };
+    const { query, ...params } = routeParams.groups;
+
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
 
     return route.handler(req, res);
   }
